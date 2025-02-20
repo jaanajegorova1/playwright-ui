54dc817c5aee7c6ea1d1395253a0dfcfa742f855
jaana <jaana.jegorova@gmail.com>
2025-02-20 22:42:39 +0200

    feat:TL-15 removed only from test.only

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
   await page.goto("https://playwright.dev/community/welcome");
